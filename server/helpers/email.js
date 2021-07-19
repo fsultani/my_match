742a6cde9c327753c78726df5d5b6b673acbb405
Farid Sultani <faridsultani.ba@gmail.com>
2021-07-19 02:15:29 -0700

    Comment out Google auth code

--- server/helpers/email.js
+++ server/helpers/email.js
@@ -1,28 +1,28 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
 
-const GOOGLE_CLIENT_ID = process.env.DEVELOPMENT ?
-  require('../credentials.json').GOOGLE_CLIENT_ID : process.env.GOOGLE_CLIENT_ID;
+// const GOOGLE_CLIENT_ID = process.env.DEVELOPMENT ?
+//   require('../credentials.json').GOOGLE_CLIENT_ID : process.env.GOOGLE_CLIENT_ID;
 
-const GOOGLE_CLIENT_SECRET = process.env.DEVELOPMENT ?
-  require('../credentials.json').GOOGLE_CLIENT_SECRET : process.env.GOOGLE_CLIENT_SECRET;
+// const GOOGLE_CLIENT_SECRET = process.env.DEVELOPMENT ?
+//   require('../credentials.json').GOOGLE_CLIENT_SECRET : process.env.GOOGLE_CLIENT_SECRET;
 
-const GOOGLE_REFRESH_TOKEN = process.env.DEVELOPMENT ?
-  require('../credentials.json').GOOGLE_REFRESH_TOKEN : process.env.GOOGLE_REFRESH_TOKEN;
+// const GOOGLE_REFRESH_TOKEN = process.env.DEVELOPMENT ?
+//   require('../credentials.json').GOOGLE_REFRESH_TOKEN : process.env.GOOGLE_REFRESH_TOKEN;
 
-const OAuth2 = google.auth.OAuth2;
-const clientId = GOOGLE_CLIENT_ID;
-const clientSecret = GOOGLE_CLIENT_SECRET;
-const refreshToken = GOOGLE_REFRESH_TOKEN;
+// const OAuth2 = google.auth.OAuth2;
+// const clientId = GOOGLE_CLIENT_ID;
+// const clientSecret = GOOGLE_CLIENT_SECRET;
+// const refreshToken = GOOGLE_REFRESH_TOKEN;
 
-const redirectUri = 'https://developers.google.com/oauthplayground';
-const accessType = 'offline'; /* https://developers.google.com/identity/protocols/oauth2/web-server#creatingclient */
+// const redirectUri = 'https://developers.google.com/oauthplayground';
+// const accessType = 'offline'; /* https://developers.google.com/identity/protocols/oauth2/web-server#creatingclient */
 
-const oauth2Client = new OAuth2(clientId, clientSecret, redirectUri, accessType);
+// const oauth2Client = new OAuth2(clientId, clientSecret, redirectUri, accessType);
 
-oauth2Client.setCredentials({
-  refresh_token: refreshToken,
-});
+// oauth2Client.setCredentials({
+//   refresh_token: refreshToken,
+// });
 
 /* Use OAuth2 */
 // const transporter = nodemailer.createTransport({
