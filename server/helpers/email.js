f067514712e9a84da67fbfa3b07c98839e8f2fb0
Farid Sultani <faridsultani.ba@gmail.com>
2021-07-19 02:08:36 -0700

    Use Google credentials based on environment

--- server/helpers/email.js
+++ server/helpers/email.js
@@ -1,11 +1,20 @@
 const nodemailer = require('nodemailer');
 const { google } = require('googleapis');
-const credentials = require('../credentials.json');
+
+const GOOGLE_CLIENT_ID = process.env.DEVELOPMENT ?
+  require('../credentials.json').GOOGLE_CLIENT_ID : process.env.GOOGLE_CLIENT_ID;
+
+const GOOGLE_CLIENT_SECRET = process.env.DEVELOPMENT ?
+  require('../credentials.json').GOOGLE_CLIENT_SECRET : process.env.GOOGLE_CLIENT_SECRET;
+
+const GOOGLE_REFRESH_TOKEN = process.env.DEVELOPMENT ?
+  require('../credentials.json').GOOGLE_REFRESH_TOKEN : process.env.GOOGLE_REFRESH_TOKEN;
 
 const OAuth2 = google.auth.OAuth2;
-const clientId = credentials.GOOGLE_CLIENT_ID;
-const clientSecret = credentials.GOOGLE_CLIENT_SECRET;
-const refreshToken = credentials.GOOGLE_REFRESH_TOKEN;
+const clientId = GOOGLE_CLIENT_ID;
+const clientSecret = GOOGLE_CLIENT_SECRET;
+const refreshToken = GOOGLE_REFRESH_TOKEN;
+
 const redirectUri = 'https://developers.google.com/oauthplayground';
 const accessType = 'offline'; /* https://developers.google.com/identity/protocols/oauth2/web-server#creatingclient */
 
